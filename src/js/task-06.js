98649c0272a6a74de08bd9cd4402787466092307
Yaroslav Prokudin <jaroslav.prokudin@gmail.com>
2021-04-07 20:35:36 +0300

    corrector 100%

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,9 +1,19 @@
 const input = document.querySelector('#validation-input');
 
+input.addEventListener('focus', onInputFocus);
 input.addEventListener('blur', onInputBlur);
 
-function onInputBlur() {
-  if (input.value.length >= +input.dataset.length) {
+function onInputFocus(event) {
+  //   console.log('focus');
+}
+
+function onInputBlur(event) {
+  // console.dir(event.currentTarget);
+  // console.log('blur');
+  if (
+    event.currentTarget.getAttribute('data-length') >
+    event.currentTarget.value.length
+  ) {
     input.classList.add('valid');
   } else {
     input.classList.add('invalid');
