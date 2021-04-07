a79811849dadc14f060695d2c72227086cc3b9bf
Yaroslav Prokudin <jaroslav.prokudin@gmail.com>
2021-04-07 20:54:46 +0300

    corrector task

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,17 +1,15 @@
 const input = document.querySelector('#validation-input');
-
 input.addEventListener('focus', onInputFocus);
 input.addEventListener('blur', onInputBlur);
-
 function onInputFocus(event) {
   //   console.log('focus');
 }
-
 function onInputBlur(event) {
   // console.dir(event.currentTarget);
   // console.log('blur');
+  console.log(event.currentTarget.value.length);
   if (
-    +event.currentTarget.getAttribute('data-length') >
+    +event.currentTarget.getAttribute('data-length') ===
     event.currentTarget.value.length
   ) {
     input.classList.add('valid');
